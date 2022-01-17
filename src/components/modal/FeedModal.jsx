cb1175f097c2a948d70749c777f9d9351b31d62e
Sooyoung Yoon <ayoooo240@gmail.com>
2022-01-18 00:05:21 +0900

    ✨ FeedCard 더보기 버튼에 삭제/수정 모달 추가

---
+++ src/components/modal/FeedModal.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import FeedPopUp from './FeedPopUp';
+
+export default function FeedModal({ list, showing, showModal }) {
+  const [visiblePop, setVisiblePop] = useState(false);
+  const openPopUp = () => {
+    setVisiblePop(true);
+  };
+  const closePopUp = () => {
+    setVisiblePop(false);
+  };
+  return (
+    <ModalContainer className={showing}>
+      <CloseBar onClick={showModal} />
+      <ul>
+        {list.map((item) => (
+          <MenuItem key={item} onClick={item === '삭제' ? openPopUp : null}>
+            {item}
+          </MenuItem>
+        ))}
+      </ul>
+      {visiblePop && <FeedPopUp visible={visiblePop} onClose={closePopUp} />}
+    </ModalContainer>
+  );
+}
+
+FeedModal.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.string).isRequired,
+  showing: PropTypes.string,
+  showModal: PropTypes.func.isRequired,
+};
+FeedModal.defaultProps = {
+  showing: null,
+};
+
+const ModalContainer = styled.div`
+  position: fixed;
+  bottom: -146px;
+  left: 0;
+  right: 0;
+  padding: 36px 0 18px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  background: #fff;
+  z-index: 500;
+  transition: 0.5s all ease;
+  &.active {
+    bottom: 0px;
+  }
+`;
+
+const CloseBar = styled.button`
+  position: absolute;
+  top: 16px;
+  left: 50%;
+  width: 50px;
+  height: 4px;
+  border: none;
+  border-radius: 5px;
+  background-color: #dbdbdb;
+  transform: translateX(-50%);
+  cursor: pointer;
+`;
+
+const MenuItem = styled.li`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 46px;
+  padding: 0 26px;
+  font-size: 14px;
+  cursor: pointer;
+`;
